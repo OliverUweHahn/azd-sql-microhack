b184fffefc322d98a90ce91b5694a98a09369338
Oliver Hahn <OliverUweHahn@outlook.de>
2026-08-02 11:38:30 +0200

    bootstrap fix

--- scripts/bootstrap-legacy.ps1
+++ scripts/bootstrap-legacy.ps1
@@ -7,41 +7,11 @@ param(
 
 $ErrorActionPreference = 'Stop'
 
-Start-Sleep -Seconds 60
-
 Write-Host "Configuring SQL Firewall..."
 & .\Set-FW-ForAllInstances.ps1
 
-Start-Sleep -Seconds 120
-
 Write-Host "Restoring Team Databases..."
 
-#$username = $SysAdminUsername
-#$password = ConvertTo-SecureString $SysAdminPassword -AsPlainText -Force
-#$cred = New-Object PSCredential($username,$password)
-#Start-Process `
-#-FilePath "powershell.exe" `
-#-Credential $cred `
-#-ArgumentList "-ExecutionPolicy Bypass -File $((Get-Location).Path)\Restore-TeamDatabases.ps1 -TeamCount $TeamCount -BackupBaseUri $BackupBaseUri" `
-#-Wait
-
-#try
-#{
-#    Start-Process `
-#        -FilePath "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe" `
-#        -Credential $cred `
-#        -ArgumentList "-ExecutionPolicy Bypass -File `"$PSScriptRoot\Restore-TeamDatabases.ps1`" -TeamCount `"$TeamCount`" -BackupBaseUri `"$BackupBaseUri`"" `
-#        -Wait `
-#        -ErrorAction Stop
-#}
-#catch
-#{
-#    $_ | Format-List * -Force
-#    throw
-#}
-
-& .\Restore-TeamDatabases.ps1 `
-    -TeamCount $TeamCount `
-    -BackupBaseUri $BackupBaseUri
+& .\Restore-TeamDatabases.ps1 -TeamCount $TeamCount -BackupBaseUri $BackupBaseUri -sqlusername $SysAdminUsername -sqlpassword $SysAdminPassword
 
 Write-Host "Bootstrap completed."
--- scripts/bootstrap-newsql.ps1
+++ scripts/bootstrap-newsql.ps1
@@ -6,40 +6,11 @@ param(
 
 $ErrorActionPreference = 'Stop'
 
-Start-Sleep -Seconds 60
-
 Write-Host "Configuring SQL Firewall..."
 & .\Set-FW-ForAllInstances.ps1
 
-Start-Sleep -Seconds 120
-
 Write-Host "Restoring Sample Database..."
 
-$username = $SysAdminUsername
-$password = ConvertTo-SecureString $SysAdminPassword -AsPlainText -Force
-$cred = New-Object PSCredential($username,$password)
-#Start-Process `
-#-FilePath "powershell.exe" `
-#-Credential $cred `
-#-ArgumentList "-ExecutionPolicy Bypass -File $((Get-Location).Path)\Restore-SampleDatabases.ps1 -BackupUri $BackupUri" `
-#-Wait
-
-try
-{
-    Start-Process `
-        -FilePath "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe" `
-        -Credential $cred `
-        -ArgumentList "-ExecutionPolicy Bypass -File `"$PSScriptRoot\Restore-SampleDatabases.ps1`" -BackupUri `"$BackupUri`"" `
-        -Wait `
-        -ErrorAction Stop
-}
-catch
-{
-    $_ | Format-List * -Force
-    throw
-}
-
-#& .\Restore-SampleDatabases.ps1 `
-#    -BackupUri $BackupUri
+& .\Restore-SampleDatabases.ps1 -BackupUri $BackupUri -sqlusername $SysAdminUsername -sqlpassword $SysAdminPassword
 
 Write-Host "Bootstrap completed."
